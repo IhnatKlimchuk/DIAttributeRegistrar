79422cb455fea7958b0757e8994a67765b8e3146
Ihnat.Klimchuk <Ihnat.Klimchuk@gmail.com>
2017-06-12 18:18:34 +0300

    Initial commit

---
+++ AttributeDependencyInjection/RegisterAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
+    public class RegisterAttribute : Attribute
+    {
+        public ServiceLifetime ServiceLifetime { get; private set; }
+
+        public Type AsType { get; private set; }
+
+        public string[] Tags { get; private set; }
+
+        public RegisterAttribute(Type asType = null, ServiceLifetime serviceLifetime = ServiceLifetime.Scoped, params string[] tags)
+        {
+            this.ServiceLifetime = serviceLifetime;
+            this.AsType = asType;
+            this.Tags = tags;
+        }
+
+        public RegisterAttribute(ServiceLifetime serviceLifetime = ServiceLifetime.Scoped, params string[] tags) : this(null, serviceLifetime, tags)
+        {
+
+        }
+
+        public RegisterAttribute() : this(null)
+        {
+
+        }
+    }
+}
