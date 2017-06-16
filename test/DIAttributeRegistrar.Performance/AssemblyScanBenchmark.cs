1a3eb4c37c26eea54e84d1bb84e1595768e97c84
Ihnat.Klimchuk <Ihnat.Klimchuk@gmail.com>
2017-06-16 17:48:51 +0300

    New banchmark test were added.

--- test/DIAttributeRegistrar.Performance/AssemblyScanBenchmark.cs
+++ test/DIAttributeRegistrar.Performance/AssemblyScanBenchmark.cs
@@ -1,5 +1,7 @@
 ﻿using BenchmarkDotNet.Attributes;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -20,17 +22,69 @@ namespace DIAttributeRegistrar.Performance
         public void WithSpecifiedAssemblies()
         {
             IServiceCollection serviceCollection = new ServiceCollection();
-            IEnumerable<Assembly> assembliesToSearch = Enumerable.Repeat(this.GetType().GetTypeInfo().Assembly, 1);
-            AttributeRegistrar attributeRegistrar = new AttributeRegistrar();
+            Func<IEnumerable<Assembly>> getSpecifiedAssemblies = () =>
+            {
+                return Enumerable.Repeat(this.GetType().GetTypeInfo().Assembly, 1);
+            };
+            AttributeRegistrar attributeRegistrar = new AttributeRegistrar(getSpecifiedAssemblies);
 
             for (int i = 0; i < OperationsPerInvoke; i++)
             {
-                attributeRegistrar.RegisterTypes(serviceCollection, assembliesToSearch, null);
+                attributeRegistrar.RegisterTypes(serviceCollection, null);
                 serviceCollection.BuildServiceProvider();
             }
             
         }
-        
+
+        [Benchmark(OperationsPerInvoke = OperationsPerInvoke)]
+        public void GoingThroughAllAssemblies()
+        {
+            IServiceCollection serviceCollection = new ServiceCollection();
+            Func<IEnumerable<Assembly>> getAllAssemblies = () =>
+            {
+                return DependencyContext
+                    .Default
+                    .RuntimeLibraries
+                    .SelectMany(l => l.GetDefaultAssemblyNames(DependencyContext.Default))
+                    .Select(Assembly.Load);
+            };
+            AttributeRegistrar attributeRegistrar = new AttributeRegistrar(getAllAssemblies);
+
+            for (int i = 0; i < OperationsPerInvoke; i++)
+            {
+                attributeRegistrar.RegisterTypes(serviceCollection, null);
+                serviceCollection.BuildServiceProvider();
+            }
+
+        }
+
+        [Benchmark(OperationsPerInvoke = OperationsPerInvoke)]
+        public void GoingThroughAssembliesWithReference()
+        {
+            IServiceCollection serviceCollection = new ServiceCollection();
+            Func<IEnumerable<Assembly>> getAssembliesWithReference = () =>
+            {
+                var currentAssemblyFullName = this.GetType().GetTypeInfo().Assembly.FullName;
+                return DependencyContext
+                    .Default
+                    .RuntimeLibraries
+                    .SelectMany(l => l.GetDefaultAssemblyNames(DependencyContext.Default))
+                    .Select(Assembly.Load)
+                    .Where(t => 
+                        t.GetReferencedAssemblies()
+                            .Select(Assembly.Load)
+                            .Any(ra => 0 == String.Compare(ra.FullName, currentAssemblyFullName, StringComparison.OrdinalIgnoreCase)));
+            };
+            AttributeRegistrar attributeRegistrar = new AttributeRegistrar(getAssembliesWithReference);
+
+            for (int i = 0; i < OperationsPerInvoke; i++)
+            {
+                attributeRegistrar.RegisterTypes(serviceCollection, null);
+                serviceCollection.BuildServiceProvider();
+            }
+
+        }
+
         [Register]
         private class AssemblyScanBenchmark_TestClassA
         {
